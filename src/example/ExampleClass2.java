3ed10b1aad5d5c824118641fe416b14f9485bcfd
Johann Weiser <johann.weiser@aon.at>
2018-10-11 17:28:44 +0200

    First commit as student.

---
+++ src/example/ExampleClass2.java
@@ -0,0 +1,12 @@
+package example;
+
+public class ExampleClass2 {
+
+	public static void main(String[] args) {
+		System.out.println("Hello world!!!");
+		System.out.println("Hello world!!!");
+		System.out.println("Hello world!!!");
+		System.out.println("Hello world!!!");
+	}
+
+}
