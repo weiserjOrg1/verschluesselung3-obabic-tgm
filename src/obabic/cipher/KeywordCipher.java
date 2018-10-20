1b27b7ec6078e8013004fecf72723f4aa550f0d1
olive <olive@DESKTOP-AH2F1JR.home>
2018-10-20 20:24:48 +0200

    commit 2

--- src/obabic/cipher/KeywordCipher.java
+++ src/obabic/cipher/KeywordCipher.java
@@ -9,18 +9,30 @@ public class KeywordCipher extends MonoAlphabeticCipher{
 		super();
 		this.nAlph = "";
 		this.setKeyWord(keyWord);
-		this.newAlph();
+		this.makeAlph();
 	}
 
-	private void newAlph() {
+	private void makeAlph() {
+		String secretAlph = "";
+		String help1 = "";
+		this.keyWord = this.keyWord.toLowerCase();
 		
+		while(true) {
+			if(checkDuplicateChars(this.keyWord)) break;
+		}
+		secretAlph = this.keyWord + this.changedAlph();
+		super.setSecretAlphabet(secretAlph);
 	}
 
-	public String getnAlph() {
+	private String changedAlph() {
+		return null;
+	}
+
+	public String getNAlph() {
 		return nAlph;
 	}
 
-	public void setnAlph(String nAlph) {
+	public void setNAlph(String nAlph) {
 		this.nAlph = nAlph;
 	}
 
@@ -30,5 +42,16 @@ public class KeywordCipher extends MonoAlphabeticCipher{
 
 	private void setKeyWord(String keyWord) {
 		this.keyWord = keyWord;
+	}
+	
+	public static boolean checkDuplicateChars(CharSequence g) {
+	    for (int i = 0; i < g.length(); i++) {
+	        for (int j = i + 1; j < g.length(); j++) {
+	            if (g.charAt(i) == g.charAt(j)) {
+	                return true;
+	            }
+	        }
+	    }
+	    return false;
 	}
 }
--- src/obabic/cipher/TranspositionCipher.java
+++ src/obabic/cipher/TranspositionCipher.java
@@ -2,11 +2,27 @@ package obabic.cipher;
 
 public class TranspositionCipher implements Cipher{
 
-	private String text;
+	private String eText;
+
+	
+	public String getEText() {
+		return this.eText;
+	}
+
+	public void setEText(String eText) {
+		this.eText = eText;
+	}
 
 	@Override
 	public String encrypt(String text) {
-		return null;
+		String newText = text.replaceAll("\\s", "");
+		char[][] tTable = new char[1][newText.length()];
+		for(int i = 0; i < 1; i++) {
+			for(int j= 0; j < 1; i++) {
+				tTable[i][j] = '.';
+			}
+		}
+		return newText;
 	}
 
 	@Override
